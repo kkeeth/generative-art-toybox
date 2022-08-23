7d3a0bf46ca3b077c49c1d53f430ba8daadf5de4
kkeeth <zensin0082@gmail.com>
2022-08-23 13:30:32 +0900

    feat: play with playground

--- free_playground/app.js
+++ free_playground/app.js
@@ -20,35 +20,29 @@ function draw() {
     // drawingContext.setLineDash(arr)
     drawingContext.lineDashOffset = y - frameCount / 10
     // strokeWeight(yStep / 1.5)
-    // strokeCap(SQUARE)
+    strokeCap(SQUARE)
     fill(100, 200, 100)
+    // beginShape()
+    beginShape(TESS)
     for (let x = offset; x < width - offset; x += 80) {
       let ny =
         y + sin(y / 330 + x / 300) * 150 * sin(frameCount / 60) * cos(y / 300)
+      let nx = x
       /**
        * draw Y
        */
-      beginShape(TESS)
-      vertex(x + 10, ny + 10)
-      vertex(x + 30, ny + 10)
-
-      vertex(x + 50, ny + 40)
-
-      vertex(x + 70, ny + 10)
-      vertex(x + 90, ny + 10)
-
-      vertex(x + 60, ny + 55)
-
-      vertex(x + 60, ny + 90)
-      vertex(x + 40, ny + 90)
-
-      vertex(x + 40, ny + 55)
+      vertex(nx + 10, ny + 10)
+      vertex(nx + 30, ny + 10)
+      vertex(nx + 50, ny + 40)
+      vertex(nx + 70, ny + 10)
+      vertex(nx + 90, ny + 10)
+      vertex(nx + 60, ny + 55)
+      vertex(nx + 60, ny + 90)
+      vertex(nx + 40, ny + 90)
+      vertex(nx + 40, ny + 55)
       // vertex(x, ny + 10)
-      endShape(CLOSE)
-
-      /**
-       * draw U
-       */
     }
+    endShape(CLOSE)
+    // endShape()
   }
 }
