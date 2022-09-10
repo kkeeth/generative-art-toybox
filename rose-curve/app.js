8da787219d1f830422825605deb18ab4c978d176
kkeeth <zensin0082@gmail.com>
2022-09-10 22:41:02 +0900

    feat: colorful rose curve

--- rose-curve/app.js
+++ rose-curve/app.js
@@ -1,18 +1,35 @@
-const radius = 140
+const radius = 70
+const space = 160
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
-  stroke(200)
+  stroke(220)
+  noLoop()
 }
 
 function draw() {
-  background(180)
-  translate(width / 2, height / 2)
+  background(240)
+  for (let v = space; v < height - space / 2; v += space) {
+    for (let h = space / 1.2; h < width - space / 2; h += space) {
+      drawRose(Math.floor(random(1, 9)), Math.floor(random(1, 9)), h, v)
+    }
+  }
+}
 
+/**
+ *
+ * @param {number} n numerator
+ * @param {number} d denominator
+ * @param {number} v vertical
+ * @param {number} h horizontal
+ */
+function drawRose(n, d, v, h) {
+  push()
   beginShape()
-  fill((mouseY / height) * 200, (mouseX / width) * 200, 160)
-  for (let angle = 0; angle < TWO_PI * 3; angle += 0.01) {
-    let range = radius * sin(angle * (8 / 3))
+  translate(v, h)
+  fill(random(120, 220), random(120, 220), random(120, 220))
+  for (let angle = 0; angle < TWO_PI * d; angle += 0.01) {
+    let range = radius * sin(angle * (n / d))
 
     let x = range * cos(angle)
     let y = range * sin(angle)
@@ -20,4 +37,5 @@ function draw() {
     vertex(x, y)
   }
   endShape(CLOSE)
+  pop()
 }
