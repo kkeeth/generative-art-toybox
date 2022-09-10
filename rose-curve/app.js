12ff29cec56adb20d4a9e3aea7024fd9755793ea
kkeeth <zensin0082@gmail.com>
2022-09-10 20:17:26 +0900

    feat: change fill color with mouse pointer

--- rose-curve/app.js
+++ rose-curve/app.js
@@ -10,8 +10,9 @@ function draw() {
   translate(width / 2, height / 2)
 
   beginShape()
+  fill((mouseY / height) * 200, (mouseX / width) * 200, 160)
   for (let angle = 0; angle < TWO_PI * 3; angle += 0.01) {
-    let range = radius * sin(angle * (4 / 3))
+    let range = radius * sin(angle * (8 / 3))
 
     let x = range * cos(angle)
     let y = range * sin(angle)
