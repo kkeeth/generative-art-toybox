b9264e52ae36fb3733da1b8efd787b99ba3ceaec
kkeeth <zensin0082@gmail.com>
2022-10-05 12:26:27 +0900

    chore: updated directory

---
+++ WORKS/colorful-circles/app.js
@@ -0,0 +1,23 @@
+let cWidth = 30;
+let cHeight = 30;
+
+function setup() {
+  createCanvas(920, 700);
+  frameRate(5);
+}
+
+function draw() {
+  background(220);
+
+  for (let i = cWidth; i < width; i += cWidth * 3.5) {
+    for (let j = cHeight; j < height; j += cHeight * 3.5) {
+      noStroke();
+      fill(random(255), random(255), random(255));
+      ellipse(i, j, cWidth, cHeight);
+      fill(random(255), random(255), random(255));
+      ellipse(i + cWidth / 2, j, cWidth, cHeight);
+    }
+  }
+
+  noLoop();
+}
