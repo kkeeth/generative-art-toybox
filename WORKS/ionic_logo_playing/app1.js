ece0f2328c835b2d21529ebb86cd72853253cedc
kkeeth <zensin0082@gmail.com>
2022-10-10 23:37:15 +0900

    feat: brash up for app1.js of ionic

--- WORKS/ionic_logo_playing/app1.js
+++ WORKS/ionic_logo_playing/app1.js
@@ -1,4 +1,4 @@
-const size = 6;
+const size = 5;
 let img;
 let cnv;
 let xoff = 0;
@@ -9,45 +9,40 @@ function preload() {
 
 function setup() {
   cnv = createCanvas(windowWidth, windowHeight);
-  frameRate(10);
-  colorMode(HSB, 1);
+  // colorMode(HSB, 1);
+  // frameRate(10);
 }
 
 function draw() {
-  background(100, 0.5);
+  background(240);
   let newCanvasX = (windowWidth - img.width) / 2;
   let newCanvasY = (windowHeight - img.height) / 2;
   translate(newCanvasX, newCanvasY);
 
   for (let col = 0; col < img.width; col += size) {
     for (let row = 0; row < img.height; row += size) {
-      if (floor(millis() / 250) === col) {
-        strokeWeight(abs(cos(millis() / 500)) * size);
-      }
       let xPos = col;
       let yPos = row;
       let c = img.get(xPos, yPos);
 
-      push();
-      noFill();
-      if (
-        c.find((el) => {
-          return el !== 0;
-        })
-      ) {
-        // stroke(random() * 255, random() * 255, random() * 255);
-        // stroke(noise(xoff) * 255, noise(xoff) * 255, noise(xoff) * 255);
-        // stroke(
-        //   noise(xoff) * 255,
-        //   noise(xoff + 0.01) * 255,
-        //   noise(random()) * 255,
-        // );
-        stroke(abs(cos(xoff / 10)), 1, 1);
-      } else {
-        stroke(color(c));
-      }
+      strokeWeight(size);
+      /** change each points size */
+      // strokeWeight(size * abs(sin(radians(frameCount + col))));
+
+      /** change each points color */
+      // if (
+      //   c.find((el) => {
+      //     return el !== 0;
+      //   })
+      // ) {
+      //   stroke(abs(cos(radians(frameCount + col / size))), 0.8, 0.9);
+      //   stroke(abs(cos(radians(frameCount + row / size))), 0.8, 0.9);
+      //   stroke(abs(cos(xoff / 5)), 0.8, 0.9);
+      // } else {
+      //   stroke(color(c));
+      // }
+      stroke(color(c));
       point(xPos, yPos);
-      pop();
     }
     xoff += 0.01;
   }
