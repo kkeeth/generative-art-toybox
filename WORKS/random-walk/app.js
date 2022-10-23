c35288613fd72987172fa40ad90e7313906271dd
kkeeth <zensin0082@gmail.com>
2022-10-23 15:05:10 +0900

    refactor: ellipse size and walk rate

--- WORKS/random-walk/app.js
+++ WORKS/random-walk/app.js
@@ -1,31 +1,30 @@
 // coordinates
-let pX, pY, mX, mY
+let pX, pY, mX, mY;
 
 function setup() {
-  createCanvas(400, 400)
-  colorMode(HSB)
+  createCanvas(400, 400);
+  colorMode(HSB);
 
-  pX = width / 2
-  pY = height / 2
+  pX = width / 2;
+  pY = height / 2;
 }
 
 function draw() {
-  ellipse(pX, pY, 1)
-  stroke(pY % 360, 100, 100)
+  noStroke();
 
-  mX = random(-1, 1)
-  mY = random(-1, 1)
+  push();
+  fill(pY % 360, 100, 100);
+  ellipse(pX, pY, 3);
+  pop();
 
-  pX = pX + 3 * mX
-  pY = pY + 3 * mY
+  mX = random(-1, 1);
+  mY = random(-1, 1);
 
-  textSize(24)
+  pX = pX + 10 * mX;
+  pY = pY + 10 * mY;
 
-  push()
+  textSize(32);
 
-  noStroke()
-  rect(0, 0, 70, 40)
-  text(frameCount, 10, 30)
-
-  pop()
+  rect(0, 0, 70, 40);
+  text(frameCount, 10, 30);
 }
