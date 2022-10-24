df8b0fd296242078168adc86acd01b1c759534fa
kkeeth <zensin0082@gmail.com>
2022-10-24 15:08:53 +0900

    feat: completed dotts objects

--- WORKS/dotts_objects/app.js
+++ WORKS/dotts_objects/app.js
@@ -6,13 +6,14 @@ let dotArr = [];
 let NUM = 1000;
 let modeArr = [
   "line-V",
-  "line-H",
   "cross-+",
   "cross-X",
   "circle",
   "sin",
   "sin-circle",
+  "YUMEMI",
 ];
+let tmpMode = "sin";
 
 let P = Math.PI;
 let P2 = P * 2;
@@ -36,7 +37,7 @@ function setup() {
     });
   }
 
-  mode = modeArr[0];
+  mode = random(modeArr);
 }
 
 function draw() {
@@ -47,6 +48,19 @@ function draw() {
 
   for (let i = 0; i < dotArr.length; i++) {
     let { pos, targetPos, r, e } = dotArr[i];
+
+    if (mode === "sin") {
+      targetPos.x = map(i, 0, NUM - 1, 0, width);
+      targetPos.y =
+        cy + sin(map(i, 0, NUM - 1, 0, 1) * PI * 4 + sec) * vmin * 0.15;
+    } else if (mode === "sin-circle") {
+      let a = map(i, 0, NUM - 1, 0, P2);
+      let r =
+        vmin * 0.3 + sin(map(i, 0, NUM - 1, 0, 1) * P * 10 + sec) * vmin * 0.05;
+      targetPos.x = cx + cos(a) * r;
+      targetPos.y = cy + sin(a) * r;
+    }
+
     pos.x += (targetPos.x - pos.x) * e;
     pos.y += (targetPos.y - pos.y) * e;
 
@@ -57,14 +71,17 @@ function draw() {
     circle(pos.x + nx, pos.y + ny, r);
   }
 
-  if (frameCount % 100 === 0) switchMode();
+  if (frameCount % 100 === 0) {
+    do {
+      tmpMode = random(modeArr);
+    } while (mode === tmpMode);
+    mode = tmpMode;
+    switchMode(mode);
+  }
 }
 
-function switchMode() {
-  mode = random(modeArr);
-  let sec = millis() / 1000;
-
-  for (let { i, targetPos, e } of dotArr) {
+function switchMode(mode) {
+  for (let { i, targetPos } of dotArr) {
     let rnd = random() < 0.5;
     const tmpX = random(width);
     const tmpY = random(height);
@@ -75,7 +92,6 @@ function switchMode() {
         if (tmpX < width / 6) {
           if (tmpY < cy) {
             let coefficient = height / (width / 4);
-            console.log(coefficient);
             targetPos.x = map(tmpX, 0, width / 6, width / 24, (3 / 24) * width);
             targetPos.y =
               targetPos.x < width / 12
@@ -88,23 +104,112 @@ function switchMode() {
         }
         // U
         else if ((1 / 6) * width <= tmpX && tmpX < (2 / 6) * width) {
-          targetPos.x = cx;
-          targetPos.y = tmpY;
+          if (tmpY < (2 / 3) * height - (1 / 24) * width) {
+            if (tmpX < (3 / 12) * width) {
+              targetPos.x = (5 / 24) * width;
+              targetPos.y = map(
+                tmpY,
+                0,
+                (2 / 3) * height,
+                height / 3,
+                (2 / 3) * height - (1 / 24) * width,
+              );
+            } else {
+              targetPos.x = (7 / 24) * width;
+              targetPos.y = map(
+                tmpY,
+                0,
+                (2 / 3) * height,
+                height / 3,
+                (2 / 3) * height - (1 / 24) * width,
+              );
+            }
+          } else {
+            targetPos.x = map(
+              tmpX,
+              (1 / 6) * width,
+              (2 / 6) * width,
+              (5 / 24) * width,
+              (7 / 24) * width,
+            );
+            targetPos.y =
+              (1 / 12) * width * cos(radians(targetPos.x)) +
+              ((2 / 3) * height - (1 / 11) * width);
+          }
         }
         // M - first
         else if ((2 / 6) * width <= tmpX && tmpX < (3 / 6) * width) {
-          targetPos.x = cx;
-          targetPos.y = tmpY;
+          if (tmpX < (9 / 24) * width) {
+            targetPos.x = (9 / 24) * width;
+            targetPos.y = map(tmpY, 0, height, height / 3, (2 / 3) * height);
+          } else if ((9 / 24) * width <= tmpX && tmpX < (11 / 24) * width) {
+            targetPos.x = tmpX;
+
+            let coefficient = height / (width / 4);
+            let realXPos = targetPos.x - (2 / 6) * width;
+
+            targetPos.y =
+              targetPos.x < (5 / 12) * width
+                ? coefficient * realXPos + height / 6
+                : (5 / 6) * height - coefficient * realXPos;
+          } else {
+            targetPos.x = (11 / 24) * width;
+            targetPos.y = map(tmpY, 0, height, height / 3, (2 / 3) * height);
+          }
         }
         // E
         else if ((3 / 6) * width <= tmpX && tmpX < (4 / 6) * width) {
-          targetPos.x = cx;
-          targetPos.y = tmpY;
+          targetPos.x = map(
+            tmpX,
+            (3 / 6) * width,
+            (4 / 6) * width,
+            (13 / 24) * width,
+            (15 / 24) * width,
+          );
+
+          // vertical line
+          if (random() < 0.4) {
+            targetPos.x = (13 / 24) * width;
+            targetPos.y = map(
+              tmpY,
+              0,
+              height,
+              (1 / 3) * height,
+              (2 / 3) * height,
+            );
+          }
+          // first horizontal line
+          else if (tmpY < (1 / 3) * height) {
+            targetPos.y = (1 / 3) * height;
+          }
+          // second horizontal line
+          else if ((1 / 3) * height <= tmpY && tmpY < (2 / 3) * height) {
+            targetPos.y = cy;
+          }
+          // third horizontal line
+          else {
+            targetPos.y = (2 / 3) * height;
+          }
         }
         // M - second
         else if ((4 / 6) * width <= tmpX && tmpX < (5 / 6) * width) {
-          targetPos.x = cx;
-          targetPos.y = tmpY;
+          if (tmpX < (17 / 24) * width) {
+            targetPos.x = (17 / 24) * width;
+            targetPos.y = map(tmpY, 0, height, height / 3, (2 / 3) * height);
+          } else if ((17 / 24) * width <= tmpX && tmpX < (19 / 24) * width) {
+            targetPos.x = tmpX;
+
+            let coefficient = height / (width / 4);
+            let realXPos = targetPos.x - (4 / 6) * width;
+
+            targetPos.y =
+              targetPos.x < (9 / 12) * width
+                ? coefficient * realXPos + height / 6
+                : (5 / 6) * height - coefficient * realXPos;
+          } else {
+            targetPos.x = (19 / 24) * width;
+            targetPos.y = map(tmpY, 0, height, height / 3, (2 / 3) * height);
+          }
         }
         // I
         else {
@@ -118,21 +223,6 @@ function switchMode() {
         const realYPos = map(targetPos.x, 0, cx, 0, height);
 
         targetPos.y = targetPos.x < cx ? realYPos : 2 * height - realYPos;
-        break;
-      }
-      case "line-H": {
-        const tmpX = tmpX;
-
-        if (tmpX < width / 6) {
-          targetPos.x = width / 6;
-          targetPos.y = tmpY;
-        } else if (tmpX > (5 / 6) * width) {
-          targetPos.x = (5 / 6) * width;
-          targetPos.y = tmpY;
-        } else {
-          targetPos.x = tmpX;
-          targetPos.y = cy;
-        }
         break;
       }
       case "cross-+": {
@@ -150,21 +240,6 @@ function switchMode() {
       case "circle": {
         let a = random(P2);
         let r = vmin * 0.3;
-        targetPos.x = cx + cos(a) * r;
-        targetPos.y = cy + sin(a) * r;
-        break;
-      }
-      case "sin": {
-        targetPos.x = map(i, 0, NUM - 1, 0, width);
-        targetPos.y =
-          cy + sin(map(i, 0, NUM - 1, 0, 1) * PI * 4 + sec) * vmin * 0.15;
-        break;
-      }
-      case "sin-circle": {
-        let a = map(i, 0, NUM - 1, 0, P2);
-        let r =
-          vmin * 0.3 +
-          sin(map(i, 0, NUM - 1, 0, 1) * P * 10 + sec) * vmin * 0.05;
         targetPos.x = cx + cos(a) * r;
         targetPos.y = cy + sin(a) * r;
         break;
