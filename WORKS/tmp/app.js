07f663375876f163c3a3f8f9945fd1b89f495d1b
kkeeth <zensin0082@gmail.com>
2022-12-09 01:44:16 +0900

    feat: updated tmp

--- WORKS/tmp/app.js
+++ WORKS/tmp/app.js
@@ -1,50 +1,26 @@
-const span = 30;
-let xoff = 0.0;
-let lines = [];
-
 function setup() {
-  createCanvas((w = 600), (h = 600));
-  for (let i = -height / 2 + span; i < height / 2; i += span) {
-    lines.push(
-      new lineCircle(
-        sin(i / 100 + xoff) * 20 - 70,
-        sin(i / 100 + xoff) * 20 + 70,
-        i,
-      ),
-    );
-  }
+  createCanvas(710, 600, WEBGL);
 }
 
 function draw() {
-  background(255);
-  translate(width / 2, height / 2);
+  background(250);
+  normalMaterial();
+  rotateY(frameCount * 0.01);
 
-  for (let line of lines) {
-    line.update();
-    line.show();
-  }
-  xoff += 0.05;
-}
-
-class lineCircle {
-  constructor(start, end, y) {
-    this.start = start;
-    this.end = end;
-    this.y = y;
-  }
-
-  update() {
-    let diff = noise(random(xoff));
-    this.start += 3 * (diff - 0.5);
-    this.end += 3 * (diff - 0.5);
-  }
-
-  show() {
+  for (let j = 0; j < 5; j++) {
     push();
-    strokeWeight(15);
-    point(this.start, this.y);
-    point(this.end, this.y);
+    for (let i = 0; i < 80; i++) {
+      translate(
+        sin(frameCount * 0.001 + j) * 100,
+        sin(frameCount * 0.001 + j) * 100,
+        i * 0.1
+      );
+      //rotateZ(frameCount * 0.002);
+      push();
+      sphere(8, 6, 4);
+      pop();
+    }
     pop();
-    line(this.start, this.y, this.end, this.y);
   }
 }
+
