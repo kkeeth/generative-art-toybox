036279607daf788467e25c267a9ad424a8376b20
kkeeth <zensin0082@gmail.com>
2023-02-17 17:16:23 +0900

    refactor: four color rect boxes

--- STUDIES/FourColorRectBoxes/app.js
+++ STUDIES/FourColorRectBoxes/app.js
@@ -1,19 +1,19 @@
-const ColorPalette = Object.freeze({
-  colorT: "#50d0d0",
-  colorM: "#be1e3e",
-  colorK: "#7967c3",
-  colorH: "#ffc639",
-  colorN: "#255699",
+const cp = Object.freeze({
+  cT: "#50d0d0",
+  cM: "#be1e3e",
+  cK: "#7967c3",
+  cH: "#ffc639",
+  cN: "#255699",
 });
 
-var trails = [];
+let trails = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
-  background(color(ColorPalette.colorN));
+  background(color(cp.cN));
 
   camera(0, -400, 800);
   rotateY(frameCount * 0.02);
@@ -22,11 +22,11 @@ function draw() {
     createFourRectBox();
   }
 
-  for (var i = this.trails.length - 1; i >= 0; i--) {
-    this.trails[i].update();
-    this.trails[i].display();
-    if (this.trails[i].isFinished()) {
-      this.trails.splice(i, 1);
+  for (let i = trails.length - 1; i >= 0; i--) {
+    trails[i].update();
+    trails[i].display();
+    if (trails[i].isFinished()) {
+      trails.splice(i, 1);
     }
   }
 }
@@ -40,8 +40,7 @@ function createFourRectBox() {
       centerPos.y,
       centerPos.z - 70,
       100,
-      color(ColorPalette.colorM),
-      0,
+      color(cp.cM),
       false,
     ),
   );
@@ -51,8 +50,7 @@ function createFourRectBox() {
       centerPos.y,
       centerPos.z + 70,
       100,
-      color(ColorPalette.colorT),
-      0,
+      color(cp.cT),
       false,
     ),
   );
@@ -62,8 +60,7 @@ function createFourRectBox() {
       centerPos.y,
       centerPos.z,
       100,
-      color(ColorPalette.colorK),
-      0,
+      color(cp.cK),
       true,
     ),
   );
@@ -73,38 +70,30 @@ function createFourRectBox() {
       centerPos.y,
       centerPos.z,
       100,
-      color(ColorPalette.colorH),
-      0,
+      color(cp.cH),
       true,
     ),
   );
 }
-function RectTrail(x, y, z, len, col, a, isRotated) {
-  this.isReverse = random(1) > 0.5;
-  this.centerPos = createVector(x, y, z);
-  this.col = col;
-  this.len = len;
-  this.vertices = [];
-  this.angle = a;
-  for (let i = 0; i < 4; i++) {
-    const angle = HALF_PI * i + QUARTER_PI;
-    const vx = cos(angle + a) * this.len;
-    const vy = sin(angle + a) * this.len;
-    const vz = 0.0;
-    this.vertices.push(createVector(vx, vy, vz));
+class RectTrail {
+  constructor(x, y, z, len, col, isRotated) {
+    this.centerPos = createVector(x, y, z);
+    this.col = col;
+    this.len = len;
+    this.vertices = [];
+    for (let i = 0; i < 4; i++) {
+      const angle = HALF_PI * i + QUARTER_PI;
+      const vx = cos(angle) * this.len;
+      const vy = sin(angle) * this.len;
+      this.vertices.push(createVector(vx, vy));
+    }
+    this.pointPos = this.vertices[0].copy();
+    this.time = 0.0;
+    this.trail = [];
+    this.isRotatedTheta = isRotated && HALF_PI;
   }
-  this.pointPos = this.vertices[0].copy();
-  this.time = 0.0;
-  this.trail = [];
-  this.isRotatedTheta = isRotated
-    ? random(1) > 0.5
-      ? HALF_PI
-      : -HALF_PI
-    : random(1) > 0.5
-    ? 0
-    : PI;
 
-  this.update = function () {
+  update = () => {
     this.time += 1.0 / 30.0;
 
     if (0.0 <= this.time && this.time < 1.0) {
@@ -116,11 +105,6 @@ function RectTrail(x, y, z, len, col, a, isRotated) {
       this.pointPos.y = lerp(
         this.vertices[0].y,
         this.vertices[1].y,
-        fract(this.time),
-      );
-      this.pointPos.z = lerp(
-        this.vertices[0].z,
-        this.vertices[1].z,
         fract(this.time),
       );
       this.trail.push(this.pointPos.copy());
@@ -135,11 +119,6 @@ function RectTrail(x, y, z, len, col, a, isRotated) {
         this.vertices[2].y,
         fract(this.time),
       );
-      this.pointPos.z = lerp(
-        this.vertices[1].z,
-        this.vertices[2].z,
-        fract(this.time),
-      );
       this.trail.push(this.pointPos.copy());
     } else if (2.0 <= this.time && this.time < 3.0) {
       this.pointPos.x = lerp(
@@ -150,11 +129,6 @@ function RectTrail(x, y, z, len, col, a, isRotated) {
       this.pointPos.y = lerp(
         this.vertices[2].y,
         this.vertices[3].y,
-        fract(this.time),
-      );
-      this.pointPos.z = lerp(
-        this.vertices[2].z,
-        this.vertices[3].z,
         fract(this.time),
       );
       this.trail.push(this.pointPos.copy());
@@ -169,21 +143,13 @@ function RectTrail(x, y, z, len, col, a, isRotated) {
         this.vertices[0].y,
         fract(this.time),
       );
-      this.pointPos.z = lerp(
-        this.vertices[3].z,
-        this.vertices[0].z,
-        fract(this.time),
-      );
       this.trail.push(this.pointPos.copy());
-      this.pointAlpha = lerp(255, 0, fract(this.time));
     } else if (this.time > 7.0) {
       this.trail.splice(0, 1);
     }
   };
 
-  this.display = function () {
-    smooth(true);
-    rectMode(CENTER);
+  display = () => {
     strokeWeight(8);
     stroke(this.col);
     noFill();
@@ -191,14 +157,14 @@ function RectTrail(x, y, z, len, col, a, isRotated) {
     translate(this.centerPos.x, this.centerPos.y, this.centerPos.z);
     rotateY(this.isRotatedTheta);
     beginShape();
-    for (var i = this.trail.length - 1; i >= 0; i--) {
-      vertex(this.trail[i].x, this.trail[i].y, this.trail[i].z);
+    for (let i = this.trail.length - 1; i >= 0; i--) {
+      vertex(this.trail[i].x, this.trail[i].y);
     }
     endShape();
     pop();
   };
 
-  this.isFinished = function () {
+  isFinished = () => {
     return this.time > 4.0 && this.trail.length == 0;
   };
 }
