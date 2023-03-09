d66ac6a87fdb2bb6be80d405eaa76f1d944b69e4
kkeeth <zensin0082@gmail.com>
2023-03-10 02:13:50 +0900

    feat: translucent and rotate without rotate method

--- WORKS/gathering_of_umbrellas/app.js
+++ WORKS/gathering_of_umbrellas/app.js
@@ -8,77 +8,130 @@ const cp = [
   "#F5E9CF",
   "#7DB9B6",
 ];
+const NUM = 30;
 let f = 0;
 let n;
 let t;
 let w;
 let x, y;
-let seed;
 let isNight = true;
 let items = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
 
-  for (let i = 0; i < 20; i++) {
-    items.push({
-      x: random(-width / 2, width / 2),
-      y: random(-height / 2, height / 2),
-      s: random(0.5, 1.5),
-      v: random([-1, 1]) * random(1, 3),
-      c: random(cp),
-    });
+  for (let i = 0; i < NUM; i++) {
+    items.push(new Umbrella());
   }
 }
 
 function draw() {
-  background(isNight ? 50 : 255);
+  translate(width / 2, height / 2);
+  background(isNight ? 0 : 255);
 
   for (let item of items) {
-    drawUmbrella(item.x, item.y, item.v, item.s, item.c);
+    item.update();
+    push();
+    item.show();
+    pop();
   }
 }
 
-function drawUmbrella(initX, initY, v, s, c) {
-  translate(initX, initY);
-  rotateY(frameCount * v);
-  scale(s);
-
-  push();
-  fill(isNight ? 255 : c);
-  noStroke();
-  beginShape();
-  for (let i = 180; i < 360; i += 6) {
-    x = 100 * cos(i);
-    y = 90 * sin(i);
-    vertex(x, y);
+class Umbrella {
+  constructor() {
+    this.x = random(-width / 2, width / 2);
+    this.y = random(-height / 2, height / 2);
+    this.diff = random(-200, -80);
+    this.s = random(0.5, 1);
+    this.rv = random([-1, 1]) * random(1, 5);
+    this.fv = random(2, 5);
+    this.c = random(cp);
+    this.r = 0;
+    this.tr = 90;
+    console.log(hexToRgb(this.c));
   }
-  for (let j = 100; j > -100; j -= 40) {
-    for (let i = 360; i > 180; i -= 6) {
-      x = j - 20 + 20 * cos(i);
-      y = 20 * sin(i);
-      vertex(x, y);
+
+  update() {
+    if (this.y <= height / 2 + this.diff) {
+      this.y += this.fv;
+    } else {
+      this.r += 2;
+    }
+
+    if (this.r > this.tr) {
+      this.x = random(-width / 2, width / 2);
+      this.y = -height / 2 - 70;
+      this.r = 0;
     }
   }
-  endShape(CLOSE);
-  pop();
 
-  push();
-  stroke(isNight ? width : c);
-  strokeWeight(3);
-  // draw stick
-  line(0, -110, 0, +50);
+  show() {
+    translate(this.x, this.y);
 
-  // draw handle
-  noFill();
-  arc(-7.5, 50, 15, 20, 0, 180);
-  pop();
+    if (this.y <= height / 2 + this.diff) {
+      scale(cos(frameCount / this.rv) * this.s, this.s);
 
-  rotateY(-frameCount * v);
-  translate(-initX, -initY);
+      push();
+      stroke(hexToRgb(this.c, map(this.y, -height / 2, height / 2, 255, 0)));
+      strokeWeight(4);
+      // draw stick
+      line(0, -110, 0, 50);
+
+      // draw handle
+      noFill();
+      drawingContext.shadowBlur = 20;
+      drawingContext.shadowColor = color(isNight ? 255 : 0);
+      drawingContext.shadowOffsetX = 10;
+      drawingContext.shadowOffsetY = 20;
+      arc(-7.5, 50, 15, 20, 0, 180);
+      pop();
+
+      // draw body
+      push();
+      drawingContext.shadowBlur = 20;
+      drawingContext.shadowColor = color(isNight ? 220 : 100);
+      drawingContext.shadowOffsetX = 10;
+      drawingContext.shadowOffsetY = 20;
+      fill(hexToRgb(this.c, map(this.y, -height / 2, height / 2, 255, 80)));
+      noStroke();
+      beginShape();
+      for (let i = 180; i < 360; i += 6) {
+        x = 100 * cos(i);
+        y = 90 * sin(i);
+        vertex(x, y);
+      }
+      for (let j = 100; j > -100; j -= 40) {
+        for (let i = 360; i > 180; i -= 6) {
+          x = j - 20 + 20 * cos(i);
+          y = 20 * sin(i);
+          vertex(x, y);
+        }
+      }
+      endShape(CLOSE);
+      pop();
+    } else {
+      noFill();
+      stroke(isNight ? 255 : 0, map(this.r, 0, this.tr, 255, 0));
+
+      ellipse(0, 50, this.r, this.r * 0.4);
+    }
+  }
 }
 
 function mousePressed() {
   isNight = !isNight;
+}
+
+function hexToRgb(hex, alpha) {
+  hex = hex.replace("#", "");
+  alpha = alpha || 0;
+
+  const bigint = parseInt(hex, 16);
+
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+
+  return color(r, g, b, alpha);
 }
