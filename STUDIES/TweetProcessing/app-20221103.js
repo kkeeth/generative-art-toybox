570c55307684e4c51b24f523ce95c89061d274d5
kkeeth <zensin0082@gmail.com>
2023-03-17 14:26:08 +0900

    fix: reset tweet processing codes

--- STUDIES/TweetProcessing/app-20221103.js
+++ STUDIES/TweetProcessing/app-20221103.js
@@ -9,28 +9,21 @@ let de = [];
 
 function setup() {
   createCanvas(800, 700);
-  strokeWeight(3);
-  // frameRate(3);
-  textSize(20);
+  strokeWeight(1);
 }
 
 function draw() {
-  background(255, 9);
-  line(135, 0, (1.6 + 1.8) * 135, height);
-  line((1.6 + 1.8) * 135, 0, (1.6 + 1.8) * 135, height);
-  line(0, 81, width, 81);
-  line(0, 2 * 1.6 * 135, width, 2 * 1.6 * 135);
+  background(255, 10);
 
   const arr = $.map((v, i) => {
     stroke(i, i / 3, i / 5);
 
-    // if (v.x > 1 || v.y > 2) {
     point(v.copy().add(2, 1.6).mult(135));
-    // }
-    r = 1; //((v.x * 2 + 2.5) ^ (v.y + 2)) * 8;
-    // console.log(v);
+    r = ((v.x * 2 + 2.5) ^ (v.y + 2)) * 8;
+    //r = 8;
 
     return v.add(sin(v.y * r) / 90, cos(v.x * r) / 90);
+    //return v.add(cos(v.y * r) / 90, sin(v.x * r) / 90);
   });
 
   if (arr[num]) {
