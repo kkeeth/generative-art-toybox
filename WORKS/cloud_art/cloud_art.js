a606db876756b63d2f7d5d39309c330a18ecbba1
kkeeth <zensin0082@gmail.com>
2023-03-21 14:29:27 +0900

    fix: cloud art don't work with js

--- WORKS/cloud_art/cloud_art.js
+++ WORKS/cloud_art/cloud_art.js
@@ -1,9 +1,8 @@
-import {OPENGL} from "processing/opengl";
+let xstart, ystart;
 
 function setup() {
-  size(800, 600, OPENGL);
-  background(0);
-  sphereDetail(8);
+  createCanvas(800, 600, WEBGL);
+  // sphereDetail(8);
   noStroke();
 
   xstart = random(10);
@@ -12,18 +11,19 @@ function setup() {
 
 function draw() {
   background(0);
+  translate(-width / 2, -height / 2);
 
-  xstart += 0.01;
-  ystart += 0.01;
+  xstart += 0.03;
+  ystart += 0.03;
 
   xnoise = xstart;
   ynoise = ystart;
 
-  for (int y = 0; y <= height; y += 5) {
+  for (let y = 0; y <= height; y += 5) {
     ynoise += 0.1;
     xnoise = xstart;
 
-    for (int x = 0; x <= width; x += 5) {
+    for (let x = 0; x <= width; x += 5) {
       xnoise += 0.1;
       drawPoint(x, y, noise(xnoise, ynoise));
     }
@@ -31,12 +31,13 @@ function draw() {
 }
 
 function drawPoint(x, y, noiseFactor) {
-  pushMatrix();
-  translate(x, 25- - y, y);
+  push();
+  translate(x, 25 - -y, y);
   sphereSize = noiseFactor * 20;
-  gray = 150 + (noiseFactor * 120);
-  alpha = 150 + (noiseFactor * 120);
+  gray = 150 + noiseFactor * 120;
+  alpha = 150 + noiseFactor * 120;
   fill(gray, alpha);
   sphere(sphereSize);
-  popMatrix();
+  pop();
 }
+
