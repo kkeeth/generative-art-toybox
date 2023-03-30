4734c035340a5068ebb2aeb192558d5e0c3568d5
kkeeth <zensin0082@gmail.com>
2023-03-31 01:43:58 +0900

    feat: updated my template

--- p5-template/app.js
+++ p5-template/app.js
@@ -1,7 +1,14 @@
-function setup() { 
+function setup() {
   createCanvas(400, 400)
-} 
+}
 
-function draw() { 
+function draw() {
   background(220)
+}
+
+function keyPressed() {
+  // this will download the first 5 seconds of the animation!
+  if (key === 's') {
+    saveGif('mySketch', 5);
+  }
 }
