c5d1b6a3f6bc3cfba6cbe51d91dae3cae02d4a06
kkeeth <zensin0082@gmail.com>
2023-04-15 10:22:35 +0900

    feat: 20230415

--- DAILY/20230414/app.js
+++ DAILY/20230414/app.js
@@ -8,39 +8,55 @@ let cp = [
   "#085a9b",
   "#f477c3",
 ];
-const step = 80;
+const step = 100;
 let t = 0;
 let w;
+let xMax;
+let xCount = 5;
 
 function setup() {
-  createCanvas((W = windowHeight), W);
+  createCanvas(500, windowHeight);
   background(255);
   rectMode(CENTER);
+  fill(0, 100);
   noStroke();
-  fill(0, 200);
 
   w = width / 60;
+  xMax = width / xCount;
 
-  for (let i = 0; i < width; i += step) {
-    for (let j = 0; j < height; j += step) {
-      ellipse(i, j, 10, 10);
+  for (let j = height; j > 0; j -= xMax * 1.7) {
+    let numCircles = min(xMax, xCount) - 1;
+    let totalCirclesWidth = numCircles * xMax;
+    let remainingSpace = width - totalCirclesWidth;
+    let spacing = remainingSpace / (numCircles + 1);
 
-      let n = int(random(5, 10));
-      let wing = w / 1.5;
+    for (let i = 0; i < numCircles; i++) {
+      let n = xCount;
       let r = random(HALF_PI);
+      let fSize = xMax / 5;
+      let color1 = random(cp);
+      let color2 = random(cp);
 
       push();
-      translate(i + step / 2, j + step / 2);
+      translate(fSize * 2 + i * xMax + spacing * (i + 1), j - xMax / 2);
       rotate(r);
       for (let i = 0; i < n; i++) {
-        fill(lerpColor(color(random(cp)), color(random(cp)), i / n));
-        ellipse(1.4 * wing, 0, 2 * wing, 0.8 * wing);
+        fill(
+          lerpColor(
+            color(...hexToRgb(color1), 180),
+            color(...hexToRgb(color2), 180),
+            i / n,
+          ),
+        );
+        ellipse(1.4 * fSize, 0, 2 * fSize, 0.8 * fSize);
         rotate(TAU / n);
       }
-      fill(random(cp));
-      circle(0, 0, wing * 0.8);
+      fill(...hexToRgb(random(cp)), 200);
+      circle(0, 0, fSize /* / map(j, height, 0, 1, 2)*/);
       pop();
     }
+    xCount++;
+    xMax = width / xCount;
   }
 }
 
@@ -48,4 +64,15 @@ function keyPressed() {
   if (key == "s") {
     saveCanvas("mysketch", "png");
   }
+}
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? [
+        parseInt(result[1], 16), // r
+        parseInt(result[2], 16), // g
+        parseInt(result[3], 16), // b
+      ]
+    : null;
 }
