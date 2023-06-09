9b6cddd4f9e9be5f198a01bb6448b99e74a6ffbe
kkeeth <zensin0082@gmail.com>
2023-06-09 22:35:27 +0900

    feat: first shader app

--- WORKS/first_shader/app.js
+++ WORKS/first_shader/app.js
@@ -1,4 +1,5 @@
 let myShader;
+let g;
 
 function preload() {
   myShader = loadShader("shader.vert", "shader.frag");
@@ -6,10 +7,21 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  g = height / 8;
   noStroke();
   pixelDensity(1);
   shader(myShader);
   myShader.setUniform("u_resolution", [width, height]);
   rect(0, 0, width, height);
   resetShader();
+  smooth();
+}
+
+function draw() {
+  translate(-width / 2, -height / 2);
+  for (let i = 0; i <= width; i += g) {
+    for (let j = 0; j <= height; j += g) {
+      ellipse(i, j, g * 1.1, g * 1.1);
+    }
+  }
 }
