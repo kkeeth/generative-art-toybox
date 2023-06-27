f1963c9b5a8ef683c138a2cf4769390cd5def373
kkeeth <zensin0082@gmail.com>
2023-06-27 16:30:00 +0900

    feat: jumped each shapes in random

--- DAILY/2023/06/0626/app.js
+++ DAILY/2023/06/0626/app.js
@@ -1,45 +1,96 @@
 const NUM = 50;
 const padding = 50;
-const max = 3000;
+const shapeAmount = 8;
 let w;
+let shapes = [];
 
 function setup() {
   createCanvas((W = 720), W);
-  translate(0, height / 2);
   rectMode(CENTER);
-  background(0);
   noStroke();
   angleMode(DEGREES);
+  frameRate(20);
 
-  w = width / 10;
+  for (let i = 0; i < shapeAmount; i++) {
+    shapes.push({
+      type: "circle",
+      x: (width / shapeAmount) * i,
+      y: height / 6,
+      originY: height / 6,
+      size: 10,
+      direction: 1,
+    });
+    shapes.push({
+      type: "heart",
+      x: (width / shapeAmount) * i,
+      y: (2 * height) / 6,
+      originY: (2 * height) / 6,
+      size: 10,
+      direction: -1,
+    });
+    shapes.push({
+      type: "triangle",
+      x: (width / shapeAmount) * i,
+      y: (3 * height) / 6,
+      originY: (3 * height) / 6,
+      size: 10,
+      direction: 1,
+    });
+    shapes.push({
+      type: "rect",
+      x: (width / shapeAmount) * i,
+      y: (4 * height) / 6,
+      originY: (4 * height) / 6,
+      size: 10,
+      direction: -1,
+    });
+    shapes.push({
+      type: "circle",
+      x: (width / shapeAmount) * i,
+      y: (5 * height) / 6,
+      originY: (5 * height) / 6,
+      size: 10,
+      direction: 1,
+    });
+  }
+}
 
-  push();
-  stroke(random(200, 255));
-  // line(0, -200, width, -200);
-  // line(0, -100, width, -100);
-  // line(0, 0, width, 0);
-  // line(0, 100, width, 100);
-  // line(0, 200, width, 200);
-  pop();
-  for (let x = padding; x < width - padding; x += w) {
-    push();
-    stroke(random(200, 255));
-    // line(x + padding / 2, -height / 2, x + padding / 2, height / 2);
-    pop();
-    ellipse(x + padding / 2, -300, max / x);
-    rect(x + padding / 2, -200, (x * w) / width);
-    ellipse(x + padding / 2, -100, max / x);
-    triangle(
-      x + padding / 2,
-      -(x * w) / width / 2,
-      x + padding / 2 - (x * w) / width / 2,
-      (x * w) / width / 2,
-      x + padding / 2 + (x * w) / width / 2,
-      (x * w) / width / 2,
-    );
-    ellipse(x + padding / 2, 100, max / x);
-    drawHeart(x + padding / 2, 200, (x * w) / width);
-    ellipse(x + padding / 2, 300, max / x);
+function draw() {
+  background(0);
+
+  for (let shape of shapes) {
+    if (random() < 0.005) {
+      shape.y += random(-50, 50);
+    } else {
+      shape.y += (shape.originY - shape.y) / shapeAmount;
+    }
+    shape.x += shape.direction * 5;
+    shape.size += abs(shape.direction) * 0.5;
+
+    if (shape.type === "circle") {
+      circle(shape.x, shape.y, shape.size);
+    } else if (shape.type === "heart") {
+      drawHeart(shape.x, shape.y, shape.size);
+    } else if (shape.type === "rect") {
+      rect(shape.x, shape.y, shape.size);
+    } else if (shape.type === "triangle") {
+      triangle(
+        shape.x,
+        shape.y - shape.size / 2,
+        shape.x - shape.size / 2,
+        shape.y + shape.size / 2,
+        shape.x + shape.size / 2,
+        shape.y + shape.size / 2,
+      );
+    }
+
+    if (shape.direction === 1 && shape.x - shape.size / 2 > width) {
+      shape.x = 0;
+      shape.size = 10;
+    } else if (shape.x + shape.size / 2 < 0) {
+      shape.x = width;
+      shape.size = 10;
+    }
   }
 }
 
@@ -47,18 +98,12 @@ function drawHeart(x, y, size) {
   push();
   translate(x, y);
 
-  size = map(
-    size,
-    (padding * w) / width,
-    ((width - padding) * w) / width,
-    0.2,
-    2.2,
-  );
+  size = map(size, 0, 80, 0.2, 2.2);
   beginShape();
   for (let i = 0; i < 360; i++) {
     let dx = size * (16 * sin(i) * sin(i) * sin(i));
     let dy =
-      size * -1 * (13 * cos(i) - 5 * cos(2 * i) - 2 * cos(3 * i) - cos(4 * i));
+      -1 * size * (13 * cos(i) - 5 * cos(2 * i) - 2 * cos(3 * i) - cos(4 * i));
     vertex(dx, dy);
   }
   endShape(CLOSE);
@@ -66,7 +111,7 @@ function drawHeart(x, y, size) {
 }
 
 function keyPressed() {
-  if (key === "c") {
-    saveCanvas("mySketch", "png");
+  if (key === "s") {
+    saveGif("mySketch", 5);
   }
 }
