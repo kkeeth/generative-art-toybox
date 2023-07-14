b8d03daa61389332a342e2807a6e3aec90b0ff31
kkeeth <zensin0082@gmail.com>
2023-07-14 09:33:44 +0900

    feat: tweak

--- STUDIES/dave/CubeWave/app.js
+++ STUDIES/dave/CubeWave/app.js
@@ -9,7 +9,7 @@ function setup() {
   noStroke();
 
   ma = atan(1 / sqrt(2));
-  maxD = dist(0, 0, width / 2, height / 2);
+  maxD = dist(0, 0, width / 3, height / 3);
 }
 
 function draw() {
@@ -19,14 +19,14 @@ function draw() {
   rotateX(QUARTER_PI);
   rotateY(ma);
 
-  for (let x = 0; x < width; x += w) {
-    for (let z = 0; z < height; z += w) {
+  for (let x = 0; x < width-100; x += w) {
+    for (let z = 0; z < height-100; z += w) {
       push();
-      let d = dist(x, z, width / 2, height / 2);
+      let d = dist(x, z, width / 2 - 50, height / 2 - 50);
       let offset = map(d, 0, maxD, -PI, PI);
       let a = angle + offset;
-      let h = map(sin(a), -1, 1, 100, 600);
-      translate(x - width / 2.5, 0, z - height / 1.5);
+      let h = map(sin(a), -1, 1, 100, 500);
+      translate(x - width / 4, -height / 4, z - height / 1.5);
       normalMaterial();
       box(w - 2, h, w - 2);
       pop();
