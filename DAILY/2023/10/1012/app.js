e4076f295a4378c9cd5bc757d6a1073db3d03e91
kkeeth <zensin0082@gmail.com>
2023-10-12 14:35:21 +0900

    feat: 2023 1012

---
+++ DAILY/2023/10/1012/app.js
@@ -0,0 +1,106 @@
+const colorScheme = [
+  {
+    name: "Benedictus",
+    colors: ["#F27EA9", "#366CD9", "#5EADF2", "#636E73", "#F2E6D8"],
+  },
+  {
+    name: "Cross",
+    colors: ["#D962AF", "#58A6A6", "#8AA66F", "#F29F05", "#F26D6D"],
+  },
+  {
+    name: "Demuth",
+    colors: ["#222940", "#D98E04", "#F2A950", "#BF3E21", "#F2F2F2"],
+  },
+  {
+    name: "Hiroshige",
+    colors: ["#1B618C", "#55CCD9", "#F2BC57", "#F2DAAC", "#F24949"],
+  },
+  {
+    name: "Hokusai",
+    colors: ["#074A59", "#F2C166", "#F28241", "#F26B5E", "#F2F2F2"],
+  },
+  {
+    name: "Hokusai Blue",
+    colors: ["#023059", "#459DBF", "#87BF60", "#D9D16A", "#F2F2F2"],
+  },
+  {
+    name: "Java",
+    colors: ["#632973", "#02734A", "#F25C05", "#F29188", "#F2E0DF"],
+  },
+  {
+    name: "Kandinsky",
+    colors: ["#8D95A6", "#0A7360", "#F28705", "#D98825", "#F2F2F2"],
+  },
+  {
+    name: "Monet",
+    colors: ["#4146A6", "#063573", "#5EC8F2", "#8C4E03", "#D98A29"],
+  },
+  {
+    name: "Nizami",
+    colors: ["#034AA6", "#72B6F2", "#73BFB1", "#F2A30F", "#F26F63"],
+  },
+  {
+    name: "Renoir",
+    colors: ["#303E8C", "#F2AE2E", "#F28705", "#D91414", "#F2F2F2"],
+  },
+  {
+    name: "VanGogh",
+    colors: ["#424D8C", "#84A9BF", "#C1D9CE", "#F2B705", "#F25C05"],
+  },
+  {
+    name: "Mono",
+    colors: ["#D9D7D8", "#3B5159", "#5D848C", "#7CA2A6", "#262321"],
+  },
+  {
+    name: "RiverSide",
+    colors: ["#906FA6", "#025951", "#252625", "#D99191", "#F2F2F2"],
+  },
+];
+const NUM = 50;
+const items = [];
+let colorPattern;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1);
+  noStroke();
+  colorPattern = random(colorScheme).colors;
+
+  for (let i = 0; i < NUM; i++) {
+    items.push({
+      x: random(width),
+      y: random(height),
+      size: random(5, 100),
+      color: random(colorPattern),
+      initAngleX: random(TAU),
+      initAngleY: random(TAU),
+    });
+  }
+}
+
+function draw() {
+  background(255, 10);
+
+  push();
+
+  for (let item of items) {
+    fill(item.color);
+    let x = item.x + 70 * cos(item.initAngleX + frameCount / 17);
+    let y = item.y + 70 * sin(item.initAngleY + frameCount / 29);
+    item.size += 1.2;
+    ellipse(x, y, item.size);
+
+    if (item.size > 150) {
+      item.x = random(width);
+      item.y = random(height);
+      item.size = random(5, 20);
+    }
+  }
+  pop();
+}
+
+function keyPressed() {
+  if (key === "s") {
+    saveGif("mySketch", 3);
+  }
+}
