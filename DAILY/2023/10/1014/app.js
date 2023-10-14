ec4735e9eaee926a75290f5b28dc0d2623f8a363
kkeeth <zensin0082@gmail.com>
2023-10-14 23:56:00 +0900

    feat: updated algorithm about 1014

--- DAILY/2023/10/1014/app.js
+++ DAILY/2023/10/1014/app.js
@@ -56,73 +56,147 @@ const colorScheme = [
     colors: ["#906FA6", "#025951", "#252625", "#D99191", "#F2F2F2"],
   },
 ];
+let items = [];
 
 function setup() {
   createCanvas((W = windowHeight - 50), W);
-  background(200);
+  background(220);
   strokeWeight(5);
   noFill();
   const cp = random(colorScheme).colors;
   const span = W / 8;
+  items = new Array(W / span);
 
+  // draw arcs and diagonal lines
   for (let x = 0; x < W; x += span) {
+    items[x / span] = new Array(W / span);
     for (let y = 0; y < W; y += span) {
-      // push();
-      // stroke(255);
-      // strokeWeight(1);
-      // line(0, y, width, y);
-      // line(x, 0, x, height);
-      // pop();
+      items[x / span][y / span] = {};
 
-      stroke(random(cp));
-      let tmp = random();
-      switch (~~random(6)) {
+      items[x / span][y / span].color = random(cp);
+      items[x / span][y / span].cond = ~~random(6);
+      items[x / span][y / span].isLine = false;
+
+      isLineCondition(x / span, y / span, items[x / span][y / span].cond);
+
+      stroke(items[x / span][y / span].color);
+
+      // include conbination pattern
+      // in ←→ pattern
+      //   case 2 - 3
+      //   case 2 - 2(same cond)
+      // in ↓↑ pattern
+      //   case 4 - 5
+      //   case 2 - 2(same cond)
+
+      const { color, cond, isLine } = items[x / span][y / span];
+
+      switch (cond) {
         // left-top to right-bottom line
         case 0:
           line(x, y, x + span, y + span);
           break;
+
         // right-bottom to right-top line
         case 1:
           line(x, y + span, x + span, y);
           break;
+
         // left open half circle
         case 2:
           arc(x + span / 2, y + span / 2, span, span, -HALF_PI, HALF_PI);
-          if (tmp < 0.3) {
-            line(x, y, x + span / 2, y);
-          } else if (tmp < 0.5) {
-            line(x, y + span, x + span / 2, y + span);
-          }
           break;
+
         // right open half circle
         case 3:
           arc(x + span / 2, y + span / 2, span, span, HALF_PI, HALF_PI * 3);
-          if (tmp < 0.3) {
-            line(x + span / 2, y, x + span, y);
-          } else if (tmp < 0.5) {
-            line(x + span / 2, y + span, x + span, y + span);
-          }
           break;
+
         // top open half circle
         case 4:
           arc(x + span / 2, y + span / 2, span, span, 0, PI);
-          if (tmp < 0.3) {
-            line(x, y, x, y + span / 2);
-          } else if (tmp < 0.5) {
-            line(x + span, y, x + span, y + span / 2);
-          }
           break;
+
         // bottom open half circle
         case 5:
           arc(x + span / 2, y + span / 2, span, span, PI, TAU);
-          if (tmp < 0.3) {
-            line(x, y + span / 2, x, y + span);
-          } else if (tmp < 0.5) {
-            line(x + span, y + span / 2, x + span, y + span);
+          break;
+      }
+    }
+  }
+
+  // draw vertical and horizontal lines
+  for (let x = 0; x < W; x += span) {
+    for (let y = 0; y < W; y += span) {
+      const { color, cond, isLine } = items[x / span][y / span];
+
+      let tmp = random();
+
+      switch (cond) {
+        case 2:
+          if (isLine) {
+            stroke(color);
+            if (tmp < 0.5) {
+              line(x, y, x + span / 2, y);
+            } else if (tmp < 0.7) {
+              line(x, y + span, x + span / 2, y + span);
+            }
+          }
+          break;
+        case 3:
+          if (isLine) {
+            stroke(color);
+            if (tmp < 0.5) {
+              line(x + span / 2, y, x + span, y);
+            } else if (tmp < 0.7) {
+              line(x + span / 2, y + span, x + span, y + span);
+            }
+          }
+          break;
+        case 4:
+          if (isLine) {
+            stroke(color);
+            if (tmp < 0.5) {
+              line(x, y, x, y + span / 2);
+            } else if (tmp < 0.7) {
+              line(x + span, y, x + span, y + span / 2);
+            }
+          }
+          break;
+        case 5:
+          if (isLine) {
+            stroke(color);
+            if (tmp < 0.5) {
+              line(x, y + span / 2, x, y + span);
+            } else if (tmp < 0.7) {
+              line(x + span, y + span / 2, x + span, y + span);
+            }
           }
           break;
       }
     }
+  }
+}
+
+function isLineCondition(x, y, cond) {
+  if (x - 1 < 0 || y - 1 < 0) {
+    items[x][y].isLine = false;
+    return;
+  }
+
+  // same cond pattern
+  if (items[x - 1][y].cond === cond) {
+    items[x][y].isLine = true;
+  }
+
+  // 2-3 pattern
+  if (items[x - 1][y].cond === 2 && cond === 3) {
+    items[x][y].isLine = true;
+  }
+
+  // 4-5 pattern
+  if (items[x][y - 1].cond === 4 && cond === 5) {
+    items[x][y].isLine = true;
   }
 }
 
