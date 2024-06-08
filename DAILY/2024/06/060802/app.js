4070bbbced73c90d5963a7de22fd5b44f253e84f
Keeth Kuwahara <kiyohito.kuwahara@kaminashi.com>
2024-06-08 21:48:22 +0900

    feat: tweak

--- DAILY/2024/06/060802/app.js
+++ DAILY/2024/06/060802/app.js
@@ -8,28 +8,36 @@ let colors = [
   '#f477c3',
 ]
 let SEED = Math.floor(Math.random() * 1000)
+let t = 0
 
 function setup() {
   createCanvas((W = windowHeight - 80), W)
+}
+
+function draw() {
   background(0)
   translate(W / 2, W / 2)
 
-  for (let l = 0; l < W / 2; l += 1) {
-    for (let a = 0; a < TAU; a += PI / 180) {
+  for (let l = 0; l < W / 2.5; l += 3) {
+    for (let a = 0; a < TAU; a += PI / 240) {
       let x = l * cos(a)
       let y = l * sin(a)
-      let d = dist(0, 0, x, y)
-      let n = noise(x / 100 + SEED, y / 100 + SEED)
+      let n = noise(x / 100 + SEED + t / 99, y / 100 + SEED + t / 77)
       let c = colors[floor(n * colors.length)]
       stroke(c)
       strokeWeight(2)
       point(x, y)
     }
   }
+  t++
 }
 
 function keyPressed() {
-  if (key == 's') {
+  if (key === 's') {
+    saveGif('myGif', 5)
+  }
+
+  if (key == 'c') {
     saveCanvas('myCanvas', 'png')
   }
 }
