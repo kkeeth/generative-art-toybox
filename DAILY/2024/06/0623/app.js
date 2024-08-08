bd3c3f4eb86640b6bc2398642f7231599b757538
Keeth Kuwahara <kiyohito.kuwahara@kaminashi.com>
2024-08-08 23:48:05 +0900

    refactor

--- DAILY/2024/06/0623/app.js
+++ DAILY/2024/06/0623/app.js
@@ -1,33 +1,49 @@
+let seed
+let xiff = 0
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
+  noStroke()
+  noLoop()
+  seed = random(1000)
+}
+
+function draw() {
   background(255)
+  randomSeed(seed)
 
   // 水面を描画
   drawWaterSurface()
 }
 
 function drawWaterSurface() {
-  let lineCount = 300 // 描画する線の数
+  let lineCount = 6 // 描画する線の数
   let noiseScaleX = 0.1 // ノイズのスケール（X軸）
   let noiseScaleY = 0.1 // ノイズのスケール（Y軸）
-  let maxOffset = 20 // 最大の左右のオフセット
-  const span = width / lineCount
+  let maxOffset = 100 // 最大の上下のオフセット
+  const span = height / lineCount
 
   noFill()
-  for (let i = 0; i < width; i += span) {
-    let startX = i
-    let startY = 0
+  for (let i = 0; i < height; i += span) {
+    let startX = 0
+    let startY = i + 50
 
-    stroke(random(120, 255), random(120, 255), random(120, 255))
+    fill(random(120, 255), random(120, 255), random(120, 255))
     beginShape()
-    for (let yy = startY; yy <= height; yy++) {
-      let offsetX =
-        noise(startX * noiseScaleX, yy * noiseScaleY) * maxOffset -
+    for (let xx = startX; xx <= width + span; xx += 10) {
+      let offsetY =
+        noise(startY * noiseScaleX, xx * noiseScaleY) * maxOffset -
         maxOffset / 2
-      vertex(startX + offsetX, yy)
+      vertex(
+        xx + xiff > width ? xx + xiff - width : xx + xiff,
+        startY + offsetY,
+      )
     }
+    vertex(width, height)
+    vertex(0, height)
     endShape()
   }
+  xiff++
 }
 
 function keyPressed() {
